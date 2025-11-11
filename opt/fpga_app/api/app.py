e2da43befcbd30972fc93670d2b30ed78fa1155b
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-11-11 20:33:45 +0900

    chore: update imports, init webui

--- opt/fpga_app/api/app.py
+++ opt/fpga_app/api/app.py
@@ -1,12 +1,15 @@
 from flask import Flask
-from auth import auth_bp
-from jobs import jobs_bp
-from fpga import fpga_bp
-from utils import utils_bp
+from api.auth import auth_bp
+from api.jobs import jobs_bp
+from api.fpga import fpga_bp
+from api.utils import utils_bp
+from webui import bp as webui_bp
 
 app = Flask(__name__)
 
-app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(jobs_bp, url_prefix="/jobs")
-app.register_blueprint(fpga_bp, url_prefix="/fpga")
-app.register_blueprint(utils_bp)
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(jobs_bp, url_prefix="/api/jobs")
+app.register_blueprint(fpga_bp, url_prefix="/api/fpga")
+app.register_blueprint(utils_bp, url_prefix="/api")
+
+app.register_blueprint(webui_bp)
