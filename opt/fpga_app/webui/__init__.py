ce2ef899a6017021202a9da7c564116e83ec8ea0
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-11-12 13:02:56 +0900

    feat: separate pages

--- opt/fpga_app/webui/__init__.py
+++ opt/fpga_app/webui/__init__.py
@@ -12,3 +12,18 @@ bp = Blueprint(
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+
+@bp.route("/stat/")
+def stat():
+    return render_template("stat.html")
+
+
+@bp.route("/queue/")
+def queue():
+    return render_template("queue.html")
+
+
+@bp.route("/result/")
+def result():
+    return render_template("result.html")
