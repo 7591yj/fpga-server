77891baea07e5548d7708f3ecae6ab330bca938d
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-11-18 16:47:30 +0900

    feat: add job cancel modal
    TODO: logics are not implemented.

--- opt/fpga_app/webui/static/js/index.js
+++ opt/fpga_app/webui/static/js/index.js
@@ -3,39 +3,69 @@ document.addEventListener("DOMContentLoaded", () => {
   const trigger = dropdown.querySelector("#dropdown-trigger");
   const logoutA = document.querySelector("#logout-a");
   const logoutModal = document.querySelector("#logout-modal");
-  const modalBackground = logoutModal.querySelector(".modal-background");
-  const modalClose = logoutModal.querySelector(".modal-close");
-  const cancelBtn = logoutModal.querySelector("button:nth-of-type(2)");
-  const confirmBtn = logoutModal.querySelector("button:nth-of-type(1)");
+  const logoutModalBackground = logoutModal.querySelector(".modal-background");
+  const logoutModalClose = logoutModal.querySelector(".modal-close");
+  const logoutModalCancelBtn = logoutModal.querySelector("button:nth-of-type(2)");
+  const logoutModalConfirmBtn = logoutModal.querySelector("button:nth-of-type(1)");
+  const jobCancelA = document.querySelector("#job-cancel-a");
 
-  // handle toggle dropdown
+  // JOB CANCEL MODAL
+  const jobCancelModal = document.querySelector("#job-cancel-modal");
+  const jobCancelModalBackground =
+    jobCancelModal.querySelector(".modal-background");
+  const jobCancelModalClose = jobCancelModal.querySelector(".modal-close");
+  const jobCancelModalCancelBtn = jobCancelModal.querySelector(
+    "button:nth-of-type(2)"
+  );
+  const jobCancelModalConfirmBtn = jobCancelModal.querySelector(
+    "button:nth-of-type(1)"
+  );
+
+  // handle dropdown
   trigger.addEventListener("click", (event) => {
     event.stopPropagation();
     dropdown.classList.toggle("is-active");
   });
 
-  // open modal when logout link clicked
+  document.addEventListener("click", (event) => {
+    if (!dropdown.contains(event.target)) {
+      dropdown.classList.remove("is-active");
+    }
+  });
+
+  // LOGOUT MODAL
   logoutA.addEventListener("click", (event) => {
     event.preventDefault();
     logoutModal.classList.add("is-active");
   });
 
-  // close modal on background, close icon, or cancel button
-  [modalBackground, modalClose, cancelBtn].forEach((el) => {
-    el.addEventListener("click", () => {
-      logoutModal.classList.remove("is-active");
-    });
-  });
+  [logoutModalBackground, logoutModalClose, logoutModalCancelBtn].forEach(
+    (el) => {
+      el.addEventListener("click", () => {
+        logoutModal.classList.remove("is-active");
+      });
+    }
+  );
 
-  // handle confirm logout
-  confirmBtn.addEventListener("click", () => {
+  logoutModalConfirmBtn.addEventListener("click", () => {
     window.location.href = "/logout";
   });
 
-  // handle cancel logout
-  document.addEventListener("click", (event) => {
-    if (!dropdown.contains(event.target)) {
-      dropdown.classList.remove("is-active");
+  // JOB CANCEL MODAL
+  jobCancelA.addEventListener("click", (event) => {
+    event.preventDefault();
+    jobCancelModal.classList.add("is-active");
+  });
+
+  [jobCancelModalBackground, jobCancelModalClose, jobCancelModalCancelBtn].forEach(
+    (el) => {
+      el.addEventListener("click", () => {
+        jobCancelModal.classList.remove("is-active");
+      });
     }
+  );
+
+  jobCancelModalConfirmBtn.addEventListener("click", () => {
+    // TODO: implement logic
   });
 });
